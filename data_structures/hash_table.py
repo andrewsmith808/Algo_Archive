d5386d6296d6669f0f33e643df4070358bb80780
andrewsmith808 <andrewsmith.code@gmail.com>
2022-02-04 13:21:28 -0800

    Implement _find_index

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -1,11 +1,49 @@
+from mimetypes import init
+
+
 class HashTable:
     """ Hashtable built using djb2 and double hashing.
     
     """
     def __init__(self, table_length):
         self.table = [None] * table_length
+    
+    def _find_index(self, key):
+        """Hash function using built in hash and Double Hashing for Collisions.
+        Parameters:
+            key -- the value to be hashed
+        
+        Return:
+            tuple -- (index, bool for wether it exists in the table or not)
+        """
+        hash1 = hash(key)
+        table_length = len(self.table)
+        initial_index = hash1 % table_length
+
+        if not self.table[initial_index]:
+            return (initial_index, False)
+        elif self.table[initial_index] == key:
+            return (initial_index, True) 
+        
+        # There exists a collision!
+        hash2 = hash(key + 'yuz')
+
+        move_amount = hash2 % (table_length - 1) + 1
+        index = (initial_index + move_amount) % table_length
+
+        while index != initial_index:
+            if not self.table[index]:
+                return (index, False)
+            elif self.table[index] == key:
+                return (index, True)
+            else:
+                index = (index + move_amount) % table_length
+            
+        return (-1, False) # the table is full
+
 
     def insert(self, key, value):
+        index = self.find_index(key)
         pass
 
     def search(self, key):
