966b8e0de9ae19e64e12874963ea421ec0e35f73
andrewsmith808 <andrewsmith.code@gmail.com>
2022-02-04 11:45:43 -0800

    Add HashTable Class declerations

---
+++ data_structures/hash_table.py
@@ -0,0 +1,23 @@
+class HashTable:
+    """ Hashtable built using djb2 and double hashing.
+    
+    """
+    def __init__(self, table_length):
+        self.table = [None] * table_length
+
+    def insert(self, key, value):
+        pass
+
+    def search(self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def _hash(value):
+        """Djb2 hashing function."""
+        pass
+
+
+if __name__ == "__main__":
+    pass
